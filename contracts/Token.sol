df8afcf586f1c92471f38069d39ccd0a7d22a1e4
skiran017 <skiran017@gmail.com>
2022-11-23 05:07:50 +0530

    5. Token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,6 +11,8 @@ contract Token {
 
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -20,5 +22,20 @@ contract Token {
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value, "low balance");
+        require(_to != address(0));
+
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 };
 
 describe('Token', () => {
-  let token, accounts, deployer;
+  let token, accounts, deployer, receiver;
   //gets executed before each 'it'
   beforeEach(async () => {
     //get token from blockchain
@@ -15,6 +15,7 @@ describe('Token', () => {
 
     accounts = await ethers.getSigners();
     deployer = accounts[0];
+    receiver = accounts[1];
   });
 
   describe('Deployment', () => {
@@ -42,6 +43,55 @@ describe('Token', () => {
 
     it('assigns total supply to deployer', async () => {
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+    });
+  });
+
+  describe('Sending Tokens', () => {
+    let amount, transaction, result;
+
+    describe('Success(Happy path)', () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        transaction = await token
+          .connect(deployer)
+          .transfer(receiver.address, amount);
+        result = await transaction.wait(); //wait till the tx is on chain
+      });
+
+      it('transfers token balances', async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(
+          tokens(999999899)
+        );
+        expect(await token.balanceOf(receiver.address)).to.equal(amount);
+      });
+
+      it('emits a Transfer event', async () => {
+        const event = result.events[0];
+        expect(event.event).to.equal('Transfer');
+
+        const args = event.args;
+        expect(args.from).to.equal(deployer.address);
+        expect(args.to).to.equal(receiver.address);
+        expect(args.value).to.equal(amount);
+      });
+    });
+
+    describe('Failure(Unhappy path)', () => {
+      it('rejects insufficient balances', async () => {
+        const invalidAmount = tokens(1000000000);
+        await expect(
+          token.connect(deployer).transfer(receiver.address, invalidAmount)
+        ).to.be.reverted;
+      });
+
+      it('rejects invalid recipient', async () => {
+        const amount = tokens(100);
+        await expect(
+          token
+            .connect(deployer)
+            .transfer('0x0000000000000000000000000000000000000000', amount)
+        ).to.be.reverted;
+      });
     });
   });
 });
