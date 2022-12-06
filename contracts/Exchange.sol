722764ced788f50c9221ec8bd6f6f4b5b8811fcf
skiran017 <skiran017@gmail.com>
2022-12-06 23:33:59 +0530

    feat: 12. Cancel Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -13,6 +13,7 @@ contract Exchange {
     //Orders mapping
     mapping(uint256 => _Order) public orders;
     uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(
@@ -22,6 +23,15 @@ contract Exchange {
         uint256 balance
     );
     event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timeStamp
+    );
+    event Cancel(
         uint256 id,
         address user,
         address tokenGet,
@@ -113,6 +123,32 @@ contract Exchange {
             _amountGet,
             _tokenGive,
             _amountGive,
+            block.timestamp
+        );
+    }
+
+    //Cancel Order
+    function cancelOrder(uint256 _id) public {
+        //Fetch order
+        _Order storage _order = orders[_id];
+
+        //Ensure the caller of the function is the owner of the order
+        require(address(_order.user) == msg.sender);
+
+        //Order must exist
+        require(_order.id == _id);
+
+        //Cancel the order
+        orderCancelled[_id] = true;
+
+        //Emit event
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
             block.timestamp
         );
     }
